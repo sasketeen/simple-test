41bf4aee13ff8c69f9589aef71f7e85ddcb1078e
sasketeen <stasprojdakov@icloud.com>
2025-05-31 20:28:42 +0300

    module-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,8 @@
-plugins {
-    kotlin("jvm") version "2.0.0"
-}
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    group = "org.example"
+    version = "0.0.1-SNAPSHOT"
 }
